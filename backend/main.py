40280ee1a0b6a299c47b7d5b556dcbbe9579f341
Honney9 <honneywalia@gmail.com>
2025-08-03 09:42:36 +0530

    Final Commit

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",  # Vite dev server
+        "http://localhost:5173",
+        "https://todo-ai-feedback-app.vercel.app"  # Vite dev server
     ],
     allow_credentials=True,
     allow_methods=["*"],
